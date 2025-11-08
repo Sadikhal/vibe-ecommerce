6e5dd87a5523f726d7e26271ef7142e9bfc41882
Sadikhali-pv <sadikhalipv@gmail.com>
2025-11-08 11:57:17 +0530

    cart

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
 import ListingCard from "../components/ListingCard";
 import { useSelector, useDispatch } from "react-redux";
-import { Loader} from "../components/ui/Loaders";
+import { Loader,EmptyState } from "../components/ui/Loaders";
 import { fetchProducts } from "../redux/productSlice";
 import Container from "../components/layout/Container";
+
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -14,16 +15,22 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <Container className="px-2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-5 gap-4 py-7">
+    <Container className="px-2 py-7">
       {isLoading ? (
-        <Loader />
+        <Loader />  
       ) : error ? (
         <div className="col-span-full text-center py-8">
-          <h3 className="text-red-500 font-medium">Error loading products</h3>
+          <h3 className="text-error font-medium">Error loading products</h3>
           <p className="text-gray-600">{error}</p>
         </div>
+      ) : products.length === 0 ? (
+        <EmptyState />   
       ) : (
-        products?.map((item) => <ListingCard key={item._id} data={item} />)
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
+          {products?.map((item) => (
+            <ListingCard key={item._id} data={item} />
+          ))}
+        </div>
       )}
     </Container>
   );
